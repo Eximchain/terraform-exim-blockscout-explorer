5b9855acda988809adfd50436bc7126d85007f6f
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2020-01-10 17:15:41 -0500

    Clean up vars

--- provider.tf
+++ provider.tf
@@ -1,8 +1,9 @@
 provider "aws" {
   version = "~> 2.17"
-  profile = var.aws_profile
+  region  = var.aws_region
+
+  profile    = var.aws_profile
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region  = var.aws_region
 }
 
